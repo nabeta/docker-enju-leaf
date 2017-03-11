557d967965b4cffa8ad8fdc5dc6b12726884d8b6
orangain <orangain@gmail.com>
2017-03-11 17:06:20 +0900

    Now migration and reindex is done only at once

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,43 +4,44 @@ set -e
 
 if [ "$1" = "bundle" ]; then
 	if [ ! -e .env ]; then
-		echo "No .env file found. Creating..."
+		echo "No .env file found. Start Enju-Leaf init process."
 
 		if [ "$SECRET_KEY_BASE" = "" ]; then
 			echo "Generating new secret key..."
 			SECRET_KEY_BASE=`bundle exec rake secret`
 		fi
-
 		echo "SECRET_KEY_BASE=$SECRET_KEY_BASE" >> .env
-	fi
 
-	echo "Checking if database is empty..."
-	if rails runner is_db_empty.rb; then
-		IS_DB_EMPTY=YES
-		echo "Database is empty."
-	fi
+		echo "Checking if database is empty..."
+		if rails runner is_db_empty.rb; then
+			IS_DB_EMPTY=YES
+			echo "Database is empty."
+		fi
 
-	echo "Running database migration..."
-	rake db:migrate
+		echo "Running database migration..."
+		rake db:migrate
 
-	if [ "$IS_DB_EMPTY" = "YES" ]; then
-		echo "Inserting initial data..."
-		rake enju_leaf:setup
-		rake enju_circulation:setup
-		rake enju_subject:setup
-		rake db:seed
-	fi
+		if [ "$IS_DB_EMPTY" = "YES" ]; then
+			echo "Inserting initial data..."
+			rake enju_leaf:setup
+			rake enju_circulation:setup
+			rake enju_subject:setup
+			rake db:seed
+		fi
 
-	echo "Upgrading database..."
-	rake enju_leaf:upgrade
+		echo "Upgrading database..."
+		rake enju_leaf:upgrade
 
-	if [ -z "$ENJU_SKIP_SOLR" ]; then
-		echo "Re-indexing..."
+		if [ -z "$ENJU_SKIP_SOLR" ]; then
+			echo "Re-indexing..."
 
-		rake sunspot:solr:start
-		sleep 5
-		rake sunspot:reindex
-		rake sunspot:solr:stop
+			rake sunspot:solr:start
+			sleep 5
+			rake sunspot:reindex
+			rake sunspot:solr:stop
+		fi
+
+		echo "Enju-Leaf init process completed; ready for start up."
 	fi
 fi
 
